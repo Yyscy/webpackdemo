017584634b6b06113c99cfe18aa6c8cc3cf9e444
Yyscy <2103374630@qq.com>
2022-06-27 20:02:21 +0800

    自动清除dist目录内容

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
 
     mode: 'development' || 'production' || 'none',
     // 入口
-    entry: "./src/index.js", 
+    entry: "./src/index.js",
     output: {
         // 出口路径 绝对路径
-        path: path.resolve(__dirname, "dist"), 
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js" // 出口文件名
     },
     // 插件
@@ -18,6 +20,9 @@ module.exports = {
             template: './public/index.html',
             // 生成的新的
             filename: 'index.html'
-        })
+        }),
+        // 默认情况下dist
+        // 删除的是ouput path 里配置的那个输出文件的文件夹
+        new CleanWebpackPlugin(),
     ]
 }
