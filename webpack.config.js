77e7b98c33891a8bac6d734f677a587b8574807e
Yyscy <2103374630@qq.com>
2022-06-27 19:58:56 +0800

    出入口文件配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path")
 
 module.exports = {
 
+    mode: 'development' || 'production' || 'none',
+    // 入口
+    entry: "./src/index.js", 
+    output: {
+        // 出口路径 绝对路径
+        path: path.resolve(__dirname, "dist"), 
+        filename: "bundle.js" // 出口文件名
+    },
+    // 插件
     plugins: [
         new HtmlWebpackPlugin({
             // 模板html地址
