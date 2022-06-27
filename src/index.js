1644b3769d73df63de563f9ac5f0e4ce1661e481
Yyscy <2103374630@qq.com>
2022-06-27 20:50:31 +0800

    图片配置4和5

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ import './style/index.less'
 // 引入jquery
 import $ from 'jquery'
 $('#swiper').css('background-color', 'red')
+
+// index.js
+import imgUrl from './assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+// 引入图片-使用
+import imgUrl1 from './assets/1.gif'
+const theImg = document.createElement("img")
+theImg.src = imgUrl1
+document.body.appendChild(theImg)
